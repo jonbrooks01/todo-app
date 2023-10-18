281ab19721a8eba8985561effa73459b345f8044
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-10-17 21:19:33 -0700

    worked on lab still have a few errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { darkMode } from './Themes/darkMode.js';
 import { lightMode } from './Themes/lightMode';
 import Todo from './Components/Todo';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Settings } from '@mui/icons-material';
+import Settings from './Context/Settings/index.jsx';
+import NavBar from './Components/Nav/index.jsx';
 
 export const GlobalContext = createContext(null);
 
 const App = () => {
   const [appTheme, setAppTheme] = useState('light');
-
+  const [settingData, setSettingData] = useState({
+    displayCount: 3,
+    hideCompleted: true,
+    sortWord: 'difficulty',
+  });
   useEffect(() => {
     const mode = localStorage.getItem('theme');
     setAppTheme(mode);
@@ -18,9 +26,10 @@ const App = () => {
   return (
     <GlobalContext.Provider
       value={{
-        displayCount: 3,
-        hideCompleted: true,
-        sortWord: 'difficulty',
+        displayCount: settingData.displayCount,
+        hideCompleted: settingData.hideCompleted,
+        sortWord: settingData.sortWord,
+        setSettingData,
         toggleAppTheme: () =>
           setAppTheme(appTheme === 'light' ? 'dark' : 'light'),
         appTheme,
@@ -28,7 +37,13 @@ const App = () => {
     >
       <ThemeProvider theme={appTheme === 'light' ? lightMode : darkMode}>
         <CssBaseline />
-        <Todo />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/home" element={<App />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Router>
       </ThemeProvider>
     </GlobalContext.Provider>
   );
